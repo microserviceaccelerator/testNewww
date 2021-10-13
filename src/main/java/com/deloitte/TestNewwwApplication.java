8fe199eca51e7619c4e7d00bcbbd0996d8b5e9d0
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-13 18:58:12 +0530

    Initial Commit

---
+++ src/main/java/com/deloitte/TestNewwwApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNewwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNewwwApplication.class, args);
+	}
+
+}
